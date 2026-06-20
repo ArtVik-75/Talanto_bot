0aa7d9219a5473cdb3efbb6ffaf95faffddded9b
ArtVik-75 <alypovartem@yandex.ru>
2026-06-21 00:16:20 +0300

    Debug keyboard group

--- app/keyboards/group.py
+++ app/keyboards/group.py
@@ -2,7 +2,9 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 group_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="Понедельник|Среда - 18:00")]
-        [KeyboardButton(text="Вторник|Четверг - 17:00")]
-    ]
+        [KeyboardButton(text="Понедельник|Среда - 18:00")],
+        [KeyboardButton(text="Вторник|Четверг - 17:00")],
+        [KeyboardButton(text="🏠 Главное меню")]
+    ],
+    resize_keyboard=True
 )
