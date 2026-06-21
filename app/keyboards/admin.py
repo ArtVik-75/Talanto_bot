2373efa222af4dc1d18fc38f370b5587122e9cec
ArtVik-75 <alypovartem@yandex.ru>
2026-06-21 23:20:02 +0300

    Debug admin keyboard

--- app/keyboards/admin.py
+++ app/keyboards/admin.py
@@ -2,8 +2,8 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
 admin_keyboard = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text="📋 Заявки")]
-        [KeyboardButton(text="📊 Статистика")]
+        [KeyboardButton(text="📋 Заявки")],
+        [KeyboardButton(text="📊 Статистика")],
         [KeyboardButton(text="🏠 Главное меню")]
     ],
     resize_keyboard=True
